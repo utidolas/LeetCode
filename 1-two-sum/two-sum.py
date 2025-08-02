8790bccc62dc75fd303bfe03f68dd691ac3db2b9
Caiuã Utida Garcia <115968855+utidolas@users.noreply.github.com>
2025-08-02 21:57:37 +0900

    Time: 0 ms (100.00%) | Memory: 18.9 MB (50.45%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,14 +1,30 @@
+from collections import defaultdict
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # initialize Hashmap 
+        '''
+        - create Dict to store values/indexes
+        - iterate through list using enumerate
+        - compute the targetNumber (number that add with current number in iterator == target)
+        - if targetNumber in dict, return list
+        - if not, assign the key & value in dict
+
+        [2,7,11,15]   target = 9
+
+        k  , v
+        {2 : 0,
+         7 : 1
+        }
+        '''
+
+        # init hashMap
         hashMap = defaultdict(int)
 
-        # iterate through nums list and get index (i) and value (number) for them using enumerate
         for i, number in enumerate(nums):
-            _targetNumber = target - number # get the number that add with 'number' will sum to target
+            targetNumber = target - number
 
-            # check if _targetNumber exist as a key in hashMap
-            if _targetNumber in hashMap:
-                return[hashMap[_targetNumber], i] # return the hashMap stored in the previous loop and the current
-            
-            hashMap[number] = i # assign 'i' as the value for the key 'number'
+            if targetNumber in hashMap:
+                return [hashMap[targetNumber], i]
+
+            hashMap[number] = i
+        
